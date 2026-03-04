3a2b1db4bfe0411624a44da4bf2e13c74a788fd1
Masaki Soumu <soumumasaki@gmail.com>
2026-03-04 09:07:23 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{padding:"40px", textAlign:"center"}}>
+      <h1 style={{fontSize:"32px", color:"#1d4ed8"}}>
+        ViewConfirm
+      </h1>
+      <p style={{color:"#64748b", marginTop:"16px"}}>
+        MIRAIグループ 人材教育プラットフォーム
+      </p>
+    </div>
+  );
+}
